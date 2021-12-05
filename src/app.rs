186b5357d6c3a99ccacf2f6c1509783d7c8aa212
Sergiy Kuzmenko <sergiy@browze.com>
2021-12-04 21:35:40 -0500

    Add stdin support

--- src/app.rs
+++ src/app.rs
@@ -1,21 +1,94 @@
-use std::io::BufReader;
-use std::fs::File;
 use serde_json::{Value};
+use std::fs::File;
+use std::io::{BufRead, BufReader, self};
+use std::path::PathBuf;
 
 use crate::cli::Opt;
 use crate::enums::SerdeFormat;
 use crate::readers::{JsonReader, JsonLineReader};
 
 
-pub fn run_jet(args: Opt) {
-    for path in args.files {
-        println!("Will read from {:?}", path);
-        let input_file = File::open(path).unwrap();
-        let buf_reader = Box::new(BufReader::new(input_file));
+struct StdinIterator {
+    already_open: bool
+}
 
+impl StdinIterator {
+    fn new() -> Self {
+        Self { already_open: false }
+    }
+}
+
+impl Iterator for StdinIterator {
+    type Item = Result<Box<dyn BufRead>, String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.already_open {
+            None
+        } else {
+            self.already_open = true;
+            Some(Ok(Box::new(BufReader::new(io::stdin()))))
+        }
+    }
+}
+
+
+struct FileIterator {
+    files: Vec<PathBuf>,
+    current_file: usize,
+}
+
+impl FileIterator {
+    fn new(files: Vec<PathBuf>) -> Self {
+        Self {
+            files: files,
+            current_file: 0
+        }
+    }
+}
+
+impl Iterator for FileIterator {
+    type Item = Result<Box<dyn BufRead>, String>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current_file >= self.files.len() {
+            return None;
+        }
+        let result = File::open(self.files[self.current_file].clone());
+        match result {
+            Ok(f) => {
+                self.current_file += 1;
+                return Some(Ok(Box::new(BufReader::new(f))));
+            },
+            Err(error) => {
+                let error_message = format!("{}: {}", self.files[self.current_file].to_string_lossy(), error);
+                self.current_file += 1;
+                return Some(Err(error_message));
+            }
+        };
+    }
+}
+
+
+
+pub fn run_jet(args: Opt) {
+    let input_sources: Box<dyn Iterator<Item = Result<Box<dyn BufRead>, String>>> = if args.files.len() == 0 {
+        Box::new(StdinIterator::new())
+    } else {
+        Box::new(FileIterator::new(args.files))
+    };
+
+    for source in input_sources {
+        match source {
+            Err(error) => {
+                eprintln!("{}", error);
+                continue;
+            },
+            _ => ()
+        };
+        let input = source.unwrap();
         let reader: Box<dyn Iterator<Item = Result<Value, String>>> = match args.input_format {
-            SerdeFormat::Json => Box::new(JsonReader::new(buf_reader)),
-            SerdeFormat::JsonLine => Box::new(JsonLineReader::new(buf_reader)),
+            SerdeFormat::Json => Box::new(JsonReader::new(input)),
+            SerdeFormat::JsonLine => Box::new(JsonLineReader::new(input)),
         };
         for document in reader {
             println!("PROCESSING {:?}", document);
