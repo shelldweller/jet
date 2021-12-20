9bc4ee331c0c7c32d0e3e6072ded9b0841093dd0
Sergiy Kuzmenko <sergiy@browze.com>
2021-12-19 22:12:27 -0500

    Lifetimes!

--- src/writers.rs
+++ src/writers.rs
@@ -8,13 +8,13 @@ pub trait Writer {
 }
 
 
-pub struct JsonWriter {
-    writer: Box<dyn Write>,
+pub struct JsonWriter<'a> {
+    writer: Box<dyn Write + 'a>,
     started: bool
 }
 
-impl JsonWriter {
-    pub fn new(writer: Box<dyn Write>) -> Self {
+impl<'a> JsonWriter<'a> {
+    pub fn new(writer: Box<dyn Write + 'a>) -> Self {
         Self {
             writer: writer,
             started: false
@@ -22,7 +22,7 @@ impl JsonWriter {
     }
 }
 
-impl Writer for JsonWriter {
+impl<'a> Writer for JsonWriter<'a> {
     fn write(&mut self, document: Value) {
         if self.started {
             self.writer.write(b",");
@@ -41,11 +41,11 @@ impl Writer for JsonWriter {
 }
 
 
-pub struct JsonLineWriter{
-    writer: Box<dyn Write>,
+pub struct JsonLineWriter<'a>{
+    writer: Box<dyn Write + 'a>,
 }
 
-impl JsonLineWriter {
+impl<'a> JsonLineWriter<'a> {
     pub fn new(writer: Box<dyn Write>) -> Self {
         Self {
             writer: writer,
@@ -53,7 +53,7 @@ impl JsonLineWriter {
     }
 }
 
-impl Writer for JsonLineWriter {
+impl<'a> Writer for JsonLineWriter<'a> {
     fn write(&mut self, document: Value) {
         self.writer.write(&document.to_string().into_bytes());
         self.writer.write(b"\n");
@@ -64,24 +64,25 @@ impl Writer for JsonLineWriter {
 
 #[cfg(test)]
 mod json_writer_tests {
-    use std::io::{Cursor, Seek, SeekFrom};
+    use std::io::{Cursor, Read, Seek, SeekFrom};
     use serde_json::{self, json};
     use super::{JsonWriter, Writer};
 
     #[test]
     fn write_documents() {
         let mut buffer = Cursor::new(Vec::new());
-        let doc1 = json!({"some": "doc"});
-        let doc2 = json!({"other": "doc"});
-        let mut writer = JsonWriter::new(Box::new(&mut buffer));
-        writer.write(doc1);
-        writer.write(doc2);
-        writer.done();
-
-        buffer.seek(SeekFrom::Start(0));
-        // let mut json_string = String::new();
-        // buffer.read_to_string(&mut json_string);
-        let actual: serde_json::Value = serde_json::from_reader(buffer).unwrap();
+        let mut json_string = String::new();
+        {
+            let mut writer = JsonWriter::new(Box::new(&mut buffer));
+            let doc1 = json!({"some": "doc"});
+            let doc2 = json!({"other": "doc"});
+            writer.write(doc1);
+            writer.write(doc2);
+            writer.done();
+        }
+        buffer.seek(SeekFrom::Start(0)).unwrap();
+        buffer.read_to_string(&mut json_string).unwrap();
+        let actual: serde_json::Value = serde_json::from_str(&json_string).unwrap();
         let expected = json!([{"some": "doc"}, {"other": "doc"} ]);
         assert_eq!(actual, expected);
     }
